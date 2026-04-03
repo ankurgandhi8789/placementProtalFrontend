7af7e6bee79997a276e107a6f9aa17e4b9eafa08
Ankur gandhi <ankurgandhi8789@gamil.com>
2026-04-03 22:06:12 +0530

    update url

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
-      '/api': 'http://localhost:8080',
+      '/api': 'https://placement-protal-backend.vercel.app/',
     },
   },
   optimizeDeps: {
